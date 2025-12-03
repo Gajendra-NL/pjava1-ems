861f6cac1102c28ad65d52567667a5c8e4df948e
Gajendra N L <gajendra@Gajendras-MacBook-Air.local>
2025-12-03 10:22:47 +0530

    spring test be initial commit

---
+++ src/main/java/com/gcloud/p1ems_backend/P1emsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gcloud.p1ems_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1emsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1emsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcloud/p1ems_backend/P1emsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcloud.p1ems_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1emsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
